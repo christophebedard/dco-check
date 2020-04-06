ef9dd1716814c6f6899a5af88b6ad6f0d13e4131
Christophe Bedard <bedard.christophe@gmail.com>
2020-04-05 21:08:26 -0400

    Format commits_data print
    Signed-off-by: Christophe Bedard <bedard.christophe@gmail.com>

--- dco_check.py
+++ dco_check.py
@@ -272,7 +272,7 @@ def main() -> int:
     verbose_print(f'commits: {commit_sha_before}..{commit_sha}')
 
     commits_data = get_commits_data(commit_sha_before, commit_sha)
-    verbose_print('commits_data:', commits_data)
+    verbose_print('commits_data:', ('\n' + commits_data).replace('\n', '\n\t'))
     if commits_data is None:
         return 1
 
